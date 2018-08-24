a6085ec3902f200ac4b0646c70c24c459a1c3ffb
Cesar <cesarcampos@gmail.com>
2018-08-23 23:01:57 -0300

    first

---
+++ www/ex02.php
@@ -0,0 +1,29 @@
+
+
+<?php
+//ini_set('display_errors', 1);
+
+$anoNascimmento = 1981;
+$nomeCompleto   = "Cesar de Moura" 
+
+$nome = "Dayane";
+$sobrenome = "Santos";
+
+$nomeCompleto = $nome . " " . $sobrenome;
+
+//$0abc = "teste";
+
+echo $anoNascimmento;
+echo "<br>";
+echo $nomeCompleto;
+
+unset($anoNascimmento, $nomeCompleto);
+
+echo $anoNascimmento;
+echo "<br>";
+echo $nomeCompleto;
+
+
+?>
+
+
---
+++ www/ex03.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$nome = "Cesar";
+$site = 'www.cmcinternet.com.br';
+
+$ano = 1981;
+
+$salario = 1123.13;
+
+$bloqueado = false;
+/////////////////////////////////////q
+
+$frutas = array("abacaxi", "morango", "manga");
+
+//echo $frutas[2];
+
+$nascimento = new DateTime();
+
+//var_dump($nascimento);
+
+//////////////////////////////////////////////
+
+$arquivo = fopen("ex02.php", "r");
+//var_dump($arquivo);
+
+$null = NULL;
+echo $null;
+
+?>
